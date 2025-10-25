d1824ed5af026b078c8beb1773d591eb82392f51
KAMBLI ANIMESH S. <animesh.2403414101@vcet.edu.in>
2025-10-25 21:36:00 +0530

    Add files via upload

---
+++ tapNpay/TapNpay.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tapNpay;
+
+/**
+ *
+ * @author Ani
+ */
+public class TapNpay {
+
+    public static void main(String[] args) {
+       System.out.print("Hello world");
+       LoginAndSignUp.main(new String[]{});
+       new Home().setVisible(true);
+    }
+
+    
+}
